45d6888c26b95508b24198645ac43da9e3ea8ddf
Long Tran <v.long128@gmail.com>
2015-06-02 18:19:09 -0700

    maybe this is a stable version. continuing testing...

--- events.js
+++ events.js
@@ -1,14 +1,32 @@
 var toggle = false;
 var on = false;
 
+chrome.windows.onCreated.addListener(function() {
+	chrome.browserAction.setIcon({path: "icon19-off.png"});
+	chrome.browserAction.setTitle({title: "Blink is off."});
+	toggle = false;
+	on = false;
+	console.log("create. on is " + on);
+	chrome.tabs.query({}, function(tabs) {
+		for(var i = 0; i < tabs.length; i++) {
+			if(tabs[i].url.indexOf("chrome://") == -1)	{
+				console.log("here4");
+				chrome.tabs.executeScript(tabs[i].id, {file: "content_script.js"});		
+			}
+		}
+	});
+})
+
 chrome.browserAction.onClicked.addListener(function(t) {
 	toggle = !toggle;
 	if(toggle) {
+		console.log("om");
 		chrome.browserAction.setIcon({path: "icon19.png"});
 		chrome.browserAction.setTitle({title: "Blink is on."});
 		on = true;
 	}
 	else {
+		console.log("off");
 		chrome.browserAction.setIcon({path: "icon19-off.png"});
 		chrome.browserAction.setTitle({title: "Blink is off."});
 		on = false;
@@ -16,22 +34,12 @@ chrome.browserAction.onClicked.addListener(function(t) {
 	chrome.tabs.query({}, function(tabs) {
 		for(var i = 0; i < tabs.length; i++) {
 			if(tabs[i].url.indexOf("chrome://") == -1)	{
+				console.log("running script " + i);
 				chrome.tabs.executeScript(tabs[i].id, {file: "content_script.js"});		
 			}
 		}
 	});
 });
-
-/*
-chrome.tabs.onCreated.addListener(function(tab) {
-	if(tab.url.indexOf("chrome://") == -1)
-		chrome.tabs.executeScript(tab.id, {file: "content_script.js"});
-});
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(tab.url.indexOf("chrome://") == -1)
-		chrome.tabs.executeScript(tab.id, {file: "content_script.js"});
-});*/
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if(request.cmd == "getStatus") {
